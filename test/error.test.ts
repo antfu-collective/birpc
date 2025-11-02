2ffb80f52443226c71401e51e6b535931333c06e
_Kerman <kermanx@qq.com>
2025-11-02 23:27:32 +0800

    fix: should handle error thrown by async post function

--- test/error.test.ts
+++ test/error.test.ts
@@ -86,7 +86,7 @@ it('on serialize error', async () => {
   try {
     await alice.hi('Bob')
   }
-  catch {}
+  catch { }
 
   expect(error).toMatchInlineSnapshot(`
     {
@@ -132,7 +132,7 @@ it('on parse error', async () => {
   try {
     alice.hi('Bob')
   }
-  catch {}
+  catch { }
 
   await new Promise(r => setTimeout(r, 10))
 
@@ -143,4 +143,51 @@ it('on parse error', async () => {
       "method": undefined,
     }
   `)
+})
+
+it('on async post error', async () => {
+  const channel = new MessageChannel()
+
+  let error: any
+
+  const _bob = createBirpc<AliceFunctions, BobFunctions>(
+    { ...Bob },
+    {
+      post: data => channel.port1.postMessage(data),
+      on: fn => channel.port1.on('message', fn),
+    },
+  )
+
+  const alice = createBirpc<BobFunctions, AliceFunctions>(
+    { ...Alice },
+    {
+      // mark bob's `bump` as an event without response
+      eventNames: ['bump'],
+      async post() {
+        await new Promise(r => setTimeout(r, 1))
+        throw new Error('Custom async post error')
+      },
+      on: fn => channel.port2.on('message', fn),
+      onGeneralError(err) {
+        error = err
+      },
+    },
+  )
+
+  try {
+    await alice.hi('Bob')
+  }
+  catch (e) {
+    expect(e).toStrictEqual(error)
+    expect(e).toMatchInlineSnapshot(`[Error: Custom async post error]`)
+  }
+
+
+  try {
+    await alice.bump()
+  }
+  catch (e) {
+    expect(e).toStrictEqual(error)
+    expect(e).toMatchInlineSnapshot(`[Error: Custom async post error]`)
+  }
 })
